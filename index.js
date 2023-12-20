5de0cb07440c96c6185157a9bec643a545d94460
Tanmay41 <tanmayror19@gmail.com>
2023-12-20 18:38:39 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "123456",
+  port: 5432,
+});
